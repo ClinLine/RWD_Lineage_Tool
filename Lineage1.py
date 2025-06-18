17b83f6e05422c71b471d442b17e7a73438fa232
bsnoeijer <138678352+bsnoeijer@users.noreply.github.com>
2025-06-18 09:50:51 +0000

    Correctly adding levels to nodes and input/output

--- Lineage1.py
+++ Lineage1.py
@@ -37,29 +37,14 @@ def define_levels(mapping_df, variables_df, datasets_df):
         concept = mapping_df.cell(row=i, column=1).value # id will be on first column
         required = mapping_df.cell(row=i, column=ReqCol).value
         
-        if pd.notna(required):
+        if pd.notna(required) and required != '':
             required_concepts = [req.strip() for req in required.split(",") if req.strip()
             if req.strip() and req.strip().lower() != concept.lower()]
             dependencies[concept] = required_concepts
         else:
-            dependencies[concept] = []
-    # all_nodes = set(dependencies.keys())   # set of uniquely referenced dependencies
-    # #print (dependencies)
-
-    # for i in range(3,mapping_df.max_row + 1) :        
-    #     source_variable = mapping_df.cell(row=i, column=SourceIdVarCol).value
-    #     target_variable = mapping_df.cell(row=i, column=TargetIdVarCol).value
-    #     for j in range(2,variables_df.max_row + 1) :
-    #         if source_variable== variables_df.cell(row=j, column=VarIdCol).value :
-    #             source_dataset = variables_df.cell(row=j, column=DsNameCol).value
-                
-    #             if source_dataset:
-    #                 all_nodes.add(source_dataset)
-            
-    #         if target_variable== variables_df.cell(row=j, column=VarIdCol).value :
-    #             target_dataset = variables_df.cell(row=j, column=DsNameCol).value
-    #             if target_dataset:
-    #                 all_nodes.add(target_dataset)
+            if concept != '' and concept != None:
+                dependencies[concept] = []
+    
     levels = {node: -1 for node in dependencies}
     
     for concept, required_concepts in dependencies.items():
@@ -68,6 +53,7 @@ def define_levels(mapping_df, variables_df, datasets_df):
 
     while True:
         updated = False
+        
         for concept, required_concepts in dependencies.items():
             if levels[concept] == -1:
                 if all(levels[req] != -1 for req in required_concepts):
@@ -77,32 +63,23 @@ def define_levels(mapping_df, variables_df, datasets_df):
         if not updated:
             break
 
+    mapping_concepts_by_level = sorted(dependencies.keys(), key=lambda x: levels.get(x, float('inf')))
+    print("Mapping concepts by level:", mapping_concepts_by_level)
     
-    # mapping_concepts_by_level = sorted(dependencies.keys(), key=lambda x: levels.get(x, float('inf')))
-    
-    # for concept in mapping_concepts_by_level:
-    #     for _, row in mapping_df[mapping_df['MappingConcept'] == concept].iterrows():
-    #         mapping_concept = row['MappingConcept']
-    #         source_variable = row['sourceVariables.id']
-    #         target_variable = row['targetVariable.id']
+    for i in range(3,mapping_df.max_row + 1) :        
+        mapping_concept = mapping_df.cell(row=i, column=1).value # id will be on first column
+        source_variable = mapping_df.cell(row=i, column=SourceIdVarCol).value
+        target_variable = mapping_df.cell(row=i, column=TargetIdVarCol).value
+        for j in range(2,variables_df.max_row + 1) :
+            if source_variable== variables_df.cell(row=j, column=VarIdCol).value :
+                source_dataset = variables_df.cell(row=j, column=DsNameCol).value
+                if source_dataset != '' and source_dataset != None and levels.get(source_dataset, 999) > levels.get(mapping_concept, -1) - 1:
+                    levels[source_dataset] = levels.get(mapping_concept, -1) - 1
 
-    #         source_dataset = find_dataset(source_variable, variables_df, datasets_df)
-    #         if source_dataset and levels[source_dataset] == -1:
-    #             levels[source_dataset] = levels.get(mapping_concept, -1) - 1
-
-    # for concept in mapping_concepts_by_level:
-    #     for _, row in mapping_df[mapping_df['MappingConcept'] == concept].iterrows():
-    #         mapping_concept = row['MappingConcept']
-    #         source_variable = row['sourceVariables.id']
-    #         target_variable = row['targetVariable.id']
-
-    #         target_dataset = find_dataset(target_variable, variables_df, datasets_df)
-    #         if target_dataset and levels[target_dataset] == -1:
-    #             levels[target_dataset] = levels.get(mapping_concept, 0) + 1
-
-    # for node in levels:
-    #     if levels[node] == -1:
-    #         levels[node] = 0
+            if target_variable== variables_df.cell(row=j, column=VarIdCol).value :
+                target_dataset = variables_df.cell(row=j, column=DsNameCol).value
+                if target_dataset != '' and target_dataset != None and levels.get(target_dataset, 999) > levels.get(mapping_concept, -1) - 1:
+                    levels[target_dataset] = levels.get(mapping_concept, -1) - 1
 
     return levels
 
